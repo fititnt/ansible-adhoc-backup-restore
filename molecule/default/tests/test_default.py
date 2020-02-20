996ba609e3b871dd71309d565f66d30da9d353d6
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-02-20 13:44:50 -0300

    Molecule tests: improved lint warnings; added prepare.yml

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,4 +12,28 @@ def test_hosts_file(host):
 
     assert f.exists
     assert f.user == 'root'
-    assert f.group == 'root'
+    # assert f.group == 'root'
+
+
+def test_www_site1_index_html(host):
+    f = host.file('/var/www/site-1/index.html')
+
+    assert f.exists
+    assert f.user == 'root'
+    # assert f.group == 'root'
+
+
+def test_www_site1_style_css(host):
+    f = host.file('/var/www/site-1/style.css')
+
+    assert f.exists
+    assert f.user == 'root'
+    # assert f.group == 'root'
+
+
+def test_www_site1_script_js(host):
+    f = host.file('/var/www/site-1/script.js')
+
+    assert f.exists
+    assert f.user == 'root'
+    # assert f.group == 'root'
