a3f3b1e4e0cd92cecb314f97e4b0115bc7043628
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-02-20 19:26:12 -0300

    Diver fs-default splited on drivers fs-directory (sabe behavior) and fs-file; this is need to make easier for recover without download packages first

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,7 +15,7 @@ def test_hosts_file(host):
     # assert f.group == 'root'
 
 
-def test_www_site1_index_html(host):
+def test_fs_directory_www_site1_index_html(host):
     f = host.file('/var/www/site-1/index.html')
 
     assert f.exists
@@ -24,7 +24,7 @@ def test_www_site1_index_html(host):
     assert f.content_string == 'Hello World!'
 
 
-def test_www_site1_style_css(host):
+def test_fs_directory_www_site1_style_css(host):
     f = host.file('/var/www/site-1/style.css')
 
     assert f.exists
@@ -33,10 +33,19 @@ def test_www_site1_style_css(host):
     assert f.content_string == '/* Put styles here */'
 
 
-def test_www_site1_script_js(host):
+def test_fs_directory_www_site1_script_js(host):
     f = host.file('/var/www/site-1/script.js')
 
     assert f.exists
     assert f.user == 'root'
     # assert f.group == 'root'
     assert f.content_string == '/* JavaScript code here */'
+
+
+def test_fs_file_www_singlefile_csv(host):
+    f = host.file('/var/www/single-file.csv')
+
+    assert f.exists
+    assert f.user == 'root'
+    # assert f.group == 'root'
+    assert f.content_string == 'Hello World!'
